51c4fc808f398cb889af5d39dbc8163da0c6fa32
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2020-11-05 11:24:54 +0530

    array functions header and small source

---
+++ arrayfunction.cpp/array-functions.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class MyArray
+{
+  int marks[100001];
+
+public:
+  MyArray()
+  {
+    for(int i = 0; i < 100001; i++)
+    {
+      marks[i] = -1;
+    }
+  }
+
+  MyArray([])
+  {
+
+  }
+
+  void sort();
+  void insert(int, char);
+  void push(int);
+  void append(int);
+  void insertinsort(int);
+  void inserat(int, int);
+};
+
+void sort::MyArray()
+{
+
+}
---
+++ arrayfunction.cpp/source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "array-functions.hpp"
+using namespace std;
+
+int main()
+{
+
+}
