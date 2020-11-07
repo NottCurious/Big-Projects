cb45c3bcc89b98fd8db5f572d47fcd82c61d5ce4
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2020-11-07 09:51:20 +0530

    switching to linux

--- arrayfunction.cpp/array-functions.hpp
+++ arrayfunction.cpp/array-functions.hpp
@@ -1,8 +1,34 @@
 #include <iostream>
 
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+int GetElements(int a[])
+{
+  int i = 0, failsafe = 0;
+
+  while(a[i] != -1 && failsafe < 1000)
+  {
+    i++, failsafe++;
+  }
+
+  if(failsafe == 999)
+  {
+    return -1;
+  }
+  else
+  {
+    return i;
+  }
+}
+
 class MyArray
 {
-  int marks[100001];
+  int marks[1000] = {-1};
 
 public:
   MyArray()
@@ -13,20 +39,52 @@ public:
     }
   }
 
-  MyArray([])
-  {
-
-  }
+  // MyArray([])
+  // {
+  //
+  // }
 
   void sort();
   void insert(int, char);
   void push(int);
   void append(int);
   void insertinsort(int);
-  void inserat(int, int);
+  void inserat(int);
 };
 
-void sort::MyArray()
+void MyArray::sort()
 {
+  n = GetElements(marks);
 
+  if(n == -1)
+  {
+    cout << "Error: No Space In Array";
+    return;
+  }
+
+  for(i = 0; i < n; i++)
+  {
+    for(int j = i; j < n - i - 1; j++)
+    {
+      if(a[j] > a[j + 1])
+      {
+        swap(&a[j], &a[j + 1]);
+      }
+    }
+  }
+}
+
+void MyArray::insert(int value, char f)
+{
+  switch(f)
+  {
+    case 'f':
+      push(value);
+    case 'e':
+      append(value);
+    case 'c':
+      insertinsort(value);
+    case "ia";
+      insertat(value);
+  }
 }
